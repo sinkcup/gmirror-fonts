8223a85780f44954b408b5441395c5a49ce20ed1
sinkcup <sinkcup@163.com>
2016-03-09 23:26:22 +0800

    google fonts china is down, change to ustc.edu.cn

--- app/Http/Controllers/Css.php
+++ app/Http/Controllers/Css.php
@@ -7,12 +7,12 @@ class Css extends Controller
 {
     public function show(Request $request)
     {
-        $uri = str_replace($request->root(), 'http://fonts.googleapis.com/', $request->fullUrl());
+        $uri = str_replace($request->root(), 'http://fonts.lug.ustc.edu.cn/', $request->fullUrl());
         if (\Cache::has('css')) {
             $content = \Cache::get('css');
         } else {
             $originalContent = file_get_contents($uri);
-            $content = str_replace('http://fonts.gstatic.com/', 'http://fonts-gstatic-com.gmirror.org/', $originalContent);
+            $content = str_replace('https://fonts-gstatic.lug.ustc.edu.cn/', 'http://fonts-gstatic-com.gmirror.org/', $originalContent);
             \Cache::put('css', $content, 1440);
         }
         return response($content)->header('Content-Type', 'text/css')->header('Cache-Control', 'public, max-age=86400');
