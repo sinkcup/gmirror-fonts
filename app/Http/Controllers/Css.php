a65ec6da5cb159e1fbd89f3f44ae7b2ca7db5267
sinkcup <sinkcup@163.com>
2016-03-24 15:15:14 +0800

    opt: remove hard code, use env

--- app/Http/Controllers/Css.php
+++ app/Http/Controllers/Css.php
@@ -7,12 +7,15 @@ class Css extends Controller
 {
     public function show(Request $request)
     {
-        $uri = str_replace($request->root(), 'http://fonts.lug.ustc.edu.cn/', $request->fullUrl());
+        $originCssRootUri = config('ORIGIN_CSS_ROOT_URI');
+        $uri = str_replace($request->root(), $originCssRootUri, $request->fullUrl());
         if (\Cache::has('css')) {
             $content = \Cache::get('css');
         } else {
             $originalContent = file_get_contents($uri);
-            $content = str_replace('https://fonts-gstatic.lug.ustc.edu.cn/', 'http://fonts-gstatic-com.gmirror.org/', $originalContent);
+            $originFontRootUri = config('ORIGIN_FONT_ROOT_URI');
+            $cdnFontRootUri = config('CDN_FONT_ROOT_URI');
+            $content = str_replace($originFontRootUri, $cdnFontRootUri, $originalContent);
             \Cache::put('css', $content, 1440);
         }
         return response($content)->header('Content-Type', 'text/css')->header('Cache-Control', 'public, max-age=86400');
