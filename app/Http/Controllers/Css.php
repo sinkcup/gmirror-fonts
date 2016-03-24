a20624f12e062f948a8fd0b0abac5ec0874087cc
sinkcup <sinkcup@163.com>
2016-03-24 15:38:44 +0800

    fixbug: should use env, not config

--- app/Http/Controllers/Css.php
+++ app/Http/Controllers/Css.php
@@ -7,14 +7,14 @@ class Css extends Controller
 {
     public function show(Request $request)
     {
-        $originCssRootUri = config('ORIGIN_CSS_ROOT_URI');
+        $originCssRootUri = env('ORIGIN_CSS_ROOT_URI');
         $uri = str_replace($request->root(), $originCssRootUri, $request->fullUrl());
         if (\Cache::has('css')) {
             $content = \Cache::get('css');
         } else {
             $originalContent = file_get_contents($uri);
-            $originFontRootUri = config('ORIGIN_FONT_ROOT_URI');
-            $cdnFontRootUri = config('CDN_FONT_ROOT_URI');
+            $originFontRootUri = env('ORIGIN_FONT_ROOT_URI');
+            $cdnFontRootUri = env('CDN_FONT_ROOT_URI');
             $content = str_replace($originFontRootUri, $cdnFontRootUri, $originalContent);
             \Cache::put('css', $content, 1440);
         }
